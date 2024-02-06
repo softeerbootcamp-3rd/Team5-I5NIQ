5d56738a5f2df6675dd0f7252fd0b86d6f7a313a
ThinkingDobby <chiho1221@naver.com>
2024-02-07 00:21:31 +0900

    [AOS] feature: Bottom NavBar 기능 구현

--- AOS/app/src/main/java/com/hyundai/myexperience/MainActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hyundai.myexperience
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.hyundai.myexperience.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initDataBinding()
+        setBottomNavigationView()
+
+        if (savedInstanceState == null) {
+            binding.mainBnv.selectedItemId = R.id.nav_main
+        }
+    }
+
+    private fun initDataBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
+    }
+
+    private fun setBottomNavigationView() {
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_main -> {
+                    setFragment(MainFragment())
+                    true
+                }
+                R.id.nav_program -> {
+                    setFragment(ProgramFragment())
+                    true
+                }
+                R.id.nav_schedule -> {
+                    setFragment(ScheduleFragment())
+                    true
+                }
+                R.id.nav_mypage -> {
+                    setFragment(MyPageFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.main_fl_container, fragment).commit()
     }
 }
