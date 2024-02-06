47c2cb96015a4667a965938de7cc899053e1ab0c
youngseo <youngseojang@gmail.com>
2024-02-06 15:30:41 +0900

    [AOS] feat: 로그인 페이지 디자인 적용 - 배경 이미지, 텍스트뷰, 에딧테스트, 버튼, 체크박스 적용

---
+++ AOS/app/src/main/java/com/hyundai/myexperience/SignInActivity.kt
@@ -0,0 +1,23 @@
+package com.hyundai.myexperience
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.hyundai.myexperience.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
