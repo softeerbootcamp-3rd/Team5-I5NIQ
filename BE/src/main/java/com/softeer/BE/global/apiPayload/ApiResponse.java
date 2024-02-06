2e646166538147b53cc67b8123342b426f40e436
johan1103 <johan1103@naver.com>
2024-02-06 15:45:23 +0900

    [BE] feature: ApiResponse 요청 성공 시 불필요한 파라미터 대입하지 않도록 static 팩토리 메서드 작성

--- BE/src/main/java/com/softeer/BE/global/apiPayload/ApiResponse.java
+++ BE/src/main/java/com/softeer/BE/global/apiPayload/ApiResponse.java
@@ -10,4 +10,10 @@ public class ApiResponse<T>{
   private String message;
   private Boolean isSuccess;
   private T body;
+  public static <T> ApiResponse<T> isSuccess(T body){
+    return new ApiResponse<>(ApiCode.SUCCESS, "api success", true, body);
+  }
+  public static <T> ApiResponse<T> isSuccess(){
+    return new ApiResponse<>(ApiCode.SUCCESS, "api success", true, null);
+  }
 }
