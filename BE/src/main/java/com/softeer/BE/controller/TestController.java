4bea3564b47ce97cc6b6a9a1422c06a606cf6637
isExample <yeji0331@naver.com>
2024-02-06 17:01:47 +0900

    [BE] feature: 응답 및 예외 처리 확인용 controller와 service 추가
    api 응답과 예외 처리가 의도한대로 정상 작동하는지 확인하기 위한 클래스와 로직을 추가했습니다.

--- BE/src/main/java/com/softeer/BE/controller/TestController.java
+++ BE/src/main/java/com/softeer/BE/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.softeer.BE.controller;
 
 import com.softeer.BE.global.apiPayload.ApiResponse;
+import com.softeer.BE.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
+    private final TestService testService;
+
     @GetMapping("/")
-    public ApiResponse<String> testAPI(){
+    public ApiResponse<String> exceptionAPI(@RequestParam Integer flag){
+        testService.CheckFlag(flag);
         return ApiResponse.onSuccess("성공");
     }
 }
