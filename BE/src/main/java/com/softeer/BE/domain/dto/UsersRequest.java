0540515b043a960954ce2e7021a5a0100d14270a
johan1103 <johan1103@naver.com>
2024-02-06 15:43:36 +0900

    [BE] feature: 회원가입 요청시 들어오는 http request body 파싱용 Dto 작성

--- BE/src/main/java/com/softeer/BE/domain/dto/UsersRequest.java
+++ BE/src/main/java/com/softeer/BE/domain/dto/UsersRequest.java
@@ -1,2 +1,15 @@
-package com.softeer.BE.domain.dto;public class UsersRequest {
+package com.softeer.BE.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UsersRequest {
+  @NoArgsConstructor
+  @Getter
+  public static class JoinForm{
+    private String name;
+    private String userId;
+    private String password;
+  }
 }
