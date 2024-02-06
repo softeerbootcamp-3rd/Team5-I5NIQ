3e9c3d5cc77898c4dbc7c6706f217f644658600c
johan1103 <johan1103@naver.com>
2024-02-06 15:08:15 +0900

    [BE] feature: 전역적인 에러를 handling하는 ExceptionHandler 작성

--- BE/src/main/java/com/softeer/BE/global/exception/GlobalExceptionHandler.java
+++ BE/src/main/java/com/softeer/BE/global/exception/GlobalExceptionHandler.java
@@ -1,2 +1,7 @@
-package com.softeer.BE.global.exception;public class GlobalExceptionHandler {
+package com.softeer.BE.global.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 }
