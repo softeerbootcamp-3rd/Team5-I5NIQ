679cd5d06e9df7a35c82b6d8494d5c04150feb2a
ThinkingDobby <chiho1221@naver.com>
2024-02-06 23:38:51 +0900

    [AOS] feature: Botton NavBar 구현 준비
    Data Binding 적용, menu 파일 생성

--- AOS/app/src/main/java/com/hyundai/myexperience/MainActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.hyundai.myexperience
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hyundai.myexperience.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 }
