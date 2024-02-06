59b07c705247a48e08edda863c37f1c57a12c03d
johan1103 <johan1103@naver.com>
2024-02-06 15:42:00 +0900

    [BE] feature: ID 중복 확인 위한 Response DTO 객체 작성

--- BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
+++ BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
@@ -1,2 +1,18 @@
-package com.softeer.BE.domain.dto;public class UsersResponse {
+package com.softeer.BE.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+public class UsersResponse {
+  private static final String UNIQUE = "UNIQUE";
+  private static final String DUPLICATE = "DUPLICATE";
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class UserIdDuplicated{
+    private String userIdValidation;
+    public static UserIdDuplicated of(boolean isDuplicate){
+      if(isDuplicate) return new UserIdDuplicated(DUPLICATE);
+      return new UserIdDuplicated(UNIQUE);
+    }
+  }
 }
