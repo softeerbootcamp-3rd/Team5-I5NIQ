1c394908a69f551c688cc71ea8973e290a29ff23
isExample <yeji0331@naver.com>
2024-02-07 18:10:45 +0900

    [BE] feature: 개인 프로그램 조회 service 구현
    개인이 예약한 프로그램 목록을 status 기반의 필터링을 통해 리턴합니다.

--- BE/src/main/java/com/softeer/BE/service/UserService.java
+++ BE/src/main/java/com/softeer/BE/service/UserService.java
@@ -1,21 +1,29 @@
 package com.softeer.BE.service;
 
-import com.softeer.BE.domain.dto.UsersRequest;
 import com.softeer.BE.domain.dto.UsersRequest.JoinForm;
 import com.softeer.BE.domain.dto.UsersRequest.LoginForm;
+import com.softeer.BE.domain.dto.UsersResponse;
+import com.softeer.BE.domain.entity.Participation;
 import com.softeer.BE.domain.entity.Users;
 import com.softeer.BE.repository.UsersRepository;
+import com.softeer.BE.repository.ParticipationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
   private final UsersRepository usersRepository;
+  private final ParticipationRepository participationRepository;
+
+  private static final String UPCOMING = "upcoming";
+  private static final String PAST = "past";
 
   public Boolean isDuplicated(String userId){
     Optional<Users> user = usersRepository.findById(userId);
@@ -38,5 +46,23 @@ public class UserService {
   }
   public Users findUserAfterValidation(String userId){
     return usersRepository.findById(userId).orElseThrow(()->new RuntimeException("invalid user id"));
+  }
+
+  public List<UsersResponse.ProgramList> getUserProgramList(String userId, String status) {
+      List<Participation> participationList;
+      if (UPCOMING.equals(status)) {
+          participationList = participationRepository.findUpcomingParticipationByUserId(userId);
+      } else if (PAST.equals(status)) {
+          participationList = participationRepository.findPastParticipationByUserId(userId);
+      } else {
+          participationList = participationRepository.findByUserId(userId);
+      }
+
+      return participationList.stream()
+              .map(participation -> UsersResponse.ProgramList.of(
+                      participation.getId(),
+                      participation.getSchedule().getProgram(),
+                      participation.getSchedule())
+              ).collect(Collectors.toList());
   }
 }
