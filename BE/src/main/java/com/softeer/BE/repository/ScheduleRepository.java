561e87ec5c1bc66738a85aa8a370d74a7e9b7e55
binaryrain97 <binaryrain97@gmail.com>
2024-02-07 13:40:52 +0900

    [BE] feat: 불필요한 sql 삭제 scheduleRepository의 불필요한 native sql 삭제

--- BE/src/main/java/com/softeer/BE/repository/ScheduleRepository.java
+++ BE/src/main/java/com/softeer/BE/repository/ScheduleRepository.java
@@ -11,14 +11,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    @Query(value = "SELECT DISTINCT DATE(s.start_date_time) " +
-                    "from schedule s " +
-                    "join program p on s.program_id = p.id " +
-                    "where DATE(start_date_time) < :date and p.name = :name " +
-                    "order by DATE(s.start_date_time) desc " +
-                    "limit :size", nativeQuery = true)
-    List<LocalDate> findAll(String name, LocalDate date, Long size);
-
     @Query("SELECT DISTINCT FUNCTION('DATE', s.startDateTime) " +
             "FROM Schedule s JOIN s.program p " +
             "WHERE FUNCTION('DATE', s.startDateTime) < :date AND p.name = :name " +
