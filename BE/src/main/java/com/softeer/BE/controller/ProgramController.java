43e8b169b1e56afc6b81823e56e5703ccc2b25f3
KTH <johan1103@naver.com>
2024-02-07 18:21:49 +0900

    [BE] feature: programservice 의존성 주입 및 호출

--- BE/src/main/java/com/softeer/BE/controller/ProgramController.java
+++ BE/src/main/java/com/softeer/BE/controller/ProgramController.java
@@ -2,6 +2,8 @@ package com.softeer.BE.controller;
 
 import com.softeer.BE.domain.dto.ProgramResponse.ProgramInformation;
 import com.softeer.BE.global.apiPayload.ApiResponse;
+import com.softeer.BE.service.ProgramService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/program")
+@RequiredArgsConstructor
 public class ProgramController {
+  private final ProgramService programService;
   @GetMapping("/information")
-  public ApiResponse<ProgramInformation> getDetailInformation(@RequestParam(value = "program-id")Long programId){
-    return null;
+  public ApiResponse<ProgramInformation> getDetailInformation(@RequestParam(value = "program-id")long programId){
+    return ApiResponse.onSuccess(programService.getDetailInformation(programId));
   }
 }
