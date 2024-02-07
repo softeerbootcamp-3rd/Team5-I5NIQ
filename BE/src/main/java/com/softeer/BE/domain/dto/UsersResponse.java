69884ffc7146db2ef91ab0322690cce95577dc3f
isExample <yeji0331@naver.com>
2024-02-07 17:48:50 +0900

    [BE] feature: 개인 프로그램 조회 ResponseDTO 구현
    개인이 예약한 프로그램의 목록을 조회하기 위한 응답 dto를 구현했습니다.

--- BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
+++ BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
@@ -1,8 +1,12 @@
 package com.softeer.BE.domain.dto;
 
+import com.softeer.BE.domain.entity.Program;
+import com.softeer.BE.domain.entity.Schedule;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 public class UsersResponse {
   private static final String UNIQUE = "UNIQUE";
@@ -15,6 +19,27 @@ public class UsersResponse {
     public static UserIdDuplicated of(boolean isDuplicate){
       if(isDuplicate) return new UserIdDuplicated(DUPLICATE);
       return new UserIdDuplicated(UNIQUE);
+    }
+  }
+
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Getter
+  public static class ProgramList{
+    private Long participationId;
+    private String category;
+    private String programName;
+    private String level;
+    private LocalDateTime dateTime;
+
+    public static ProgramList of(Long participationId, Program program, Schedule schedule){
+      return new ProgramList(
+              participationId,
+              program.getCategory().name(),
+              program.getName().name(),
+              program.getLevel().name(),
+              schedule.getStartDateTime()
+      );
     }
   }
 }
