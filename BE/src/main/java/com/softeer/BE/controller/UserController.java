76024fad79badec37195908d48d8f99fe959e319
isExample <yeji0331@naver.com>
2024-02-07 17:53:12 +0900

    [BE] feature: 개인 프로그램 조회 controller 구현
    개인이 예약한 프로그램을 조회하는 api의 controller를 구현했습니다.
    로그인된 사용자만 정상적으로 호출할 수 있습니다.

--- BE/src/main/java/com/softeer/BE/controller/UserController.java
+++ BE/src/main/java/com/softeer/BE/controller/UserController.java
@@ -2,15 +2,20 @@ package com.softeer.BE.controller;
 
 import com.softeer.BE.domain.dto.UsersRequest.JoinForm;
 import com.softeer.BE.domain.dto.UsersRequest.LoginForm;
+import com.softeer.BE.domain.dto.UsersResponse;
 import com.softeer.BE.domain.dto.UsersResponse.UserIdDuplicated;
 import com.softeer.BE.domain.entity.Users;
 import com.softeer.BE.global.apiPayload.ApiResponse;
+import com.softeer.BE.global.apiPayload.code.statusEnums.ErrorStatus;
+import com.softeer.BE.global.exception.GeneralHandler;
 import com.softeer.BE.global.session.UserSessionValue;
 import com.softeer.BE.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,16 +41,25 @@ public class UserController {
       throw new RuntimeException("login failure exception");
     Users user = userService.findUserAfterValidation(loginForm.getId());
     HttpSession session = request.getSession(true);
-    session.setAttribute("user",UserSessionValue.of(user));
+    session.setAttribute("user", UserSessionValue.of(user));
     return ApiResponse.onSuccess(true);
   }
-  
+
   @PostMapping("/logout")
-  public ApiResponse<Boolean> logout(HttpServletRequest request){
+  public ApiResponse<Boolean> logout(HttpServletRequest request) {
     HttpSession session = request.getSession(false);
-    if(session==null)
+    if (session == null)
       throw new RuntimeException("logout failure exception");
     session.invalidate();
     return ApiResponse.onSuccess(true);
+  }
+
+  @GetMapping("/programs")
+  public ApiResponse<List<UsersResponse.ProgramList>> listPrograms(HttpServletRequest request, @RequestParam String status) {
+    HttpSession httpSession = request.getSession(false);
+    if(httpSession == null)
+      throw new GeneralHandler(ErrorStatus._UNAUTHORIZED);
+    UserSessionValue userInfo = (UserSessionValue) httpSession.getAttribute("user");
+    return ApiResponse.onSuccess(userService.getUserProgramList(userInfo.getUserId(), status));
   }
 }
