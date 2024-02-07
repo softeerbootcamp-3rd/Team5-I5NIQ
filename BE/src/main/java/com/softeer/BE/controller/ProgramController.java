20da64678b6d43b3b9cc66641476963f5ee76a11
johan1103 <johan1103@naver.com>
2024-02-07 17:56:54 +0900

    [BE] feature: information 정보 받아오는 controller 기본 구성

--- BE/src/main/java/com/softeer/BE/controller/ProgramController.java
+++ BE/src/main/java/com/softeer/BE/controller/ProgramController.java
@@ -1,2 +1,18 @@
-package com.softeer.BE.controller;public class ProgramController {
+package com.softeer.BE.controller;
+
+import com.softeer.BE.domain.dto.ProgramResponse;
+import com.softeer.BE.domain.dto.ProgramResponse.ProgramInformation;
+import com.softeer.BE.global.apiPayload.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/program")
+public class ProgramController {
+  @GetMapping("/information")
+  public ApiResponse<ProgramInformation> getDetailInformation(@RequestParam(value = "program-id")Long programId){
+    return null;
+  }
 }
