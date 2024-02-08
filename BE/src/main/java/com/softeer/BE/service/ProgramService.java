db286429b17ad8a99ab75ca0326d24fbc8f8171d
KTH <johan1103@naver.com>
2024-02-08 18:28:33 +0900

    [BE] feature: 장소정보 불러오는 서비스 코드 작성, 공통 코드 분리
    repository에서 orElseThrow로 불러오는 로직 공통 함수로 분리

--- BE/src/main/java/com/softeer/BE/service/ProgramService.java
+++ BE/src/main/java/com/softeer/BE/service/ProgramService.java
@@ -3,6 +3,7 @@ package com.softeer.BE.service;
 import com.softeer.BE.domain.dto.ProgramResponse;
 import com.softeer.BE.domain.dto.ProgramResponse.ProgramDetail;
 import com.softeer.BE.domain.dto.ProgramResponse.ProgramInformation;
+import com.softeer.BE.domain.dto.ProgramResponse.ProgramLocations;
 import com.softeer.BE.domain.entity.Program;
 import com.softeer.BE.repository.ProgramRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProgramService {
   private final ProgramRepository programRepository;
   public ProgramInformation getDetailInformation(long programId){
-    Program program = programRepository.findById(programId).orElseThrow(()->new RuntimeException("invalid program id"));
+    Program program = findByIdOrElseThrow(programId, new RuntimeException("invalid program id"));
     return ProgramInformation.of(program);
   }
   public ProgramDetail getDetail(long programId){
-    Program program = programRepository.findById(programId).orElseThrow(()->new RuntimeException("invalid program id"));
+    Program program = findByIdOrElseThrow(programId, new RuntimeException("invalid program id"));
     return ProgramDetail.of(program);
+  }
+  public ProgramLocations getLocations(long programId){
+    Program program = findByIdOrElseThrow(programId, new RuntimeException("invalid program id"));
+    return ProgramLocations.of(program);
+  }
+  private Program findByIdOrElseThrow(long programId, RuntimeException e){
+    return programRepository.findById(programId).orElseThrow(()->e);
   }
 }
