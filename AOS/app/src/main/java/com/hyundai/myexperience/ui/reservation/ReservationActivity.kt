0b641e36b7bd99cef8c955d4dcbb8f8f8eb8427d
ThinkingDobby <chiho1221@naver.com>
2024-02-08 16:53:52 +0900

    [AOS] feature: ReservationActivity 디자인 적용

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyundai.myexperience.R
 
-class ReservationEntranceActivity : AppCompatActivity() {
+class ReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_entrance)
+        setContentView(R.layout.activity_reservation)
     }
 }
