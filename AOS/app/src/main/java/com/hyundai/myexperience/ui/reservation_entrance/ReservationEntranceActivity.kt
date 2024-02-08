a978936fcf938443d8314914f47bc8a270476518
ThinkingDobby <chiho1221@naver.com>
2024-02-09 00:24:06 +0900

    [AOS] feature: 예약 화면 연결

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation_entrance/ReservationEntranceActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation_entrance/ReservationEntranceActivity.kt
@@ -1,12 +1,34 @@
 package com.hyundai.myexperience.ui.reservation_entrance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.ActivityReservationEntranceBinding
+import com.hyundai.myexperience.ui.reservation.ReservationActivity
+import com.hyundai.myexperience.utils.navigationHeight
+import com.hyundai.myexperience.utils.setStatusBarTransparent
 
 class ReservationEntranceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReservationEntranceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_entrance)
+
+        initDataBinding()
+
+        this.setStatusBarTransparent()
+        binding.cl.setPadding(0, 0, 0, navigationHeight())
+
+        binding.reservationClProgram.setOnClickListener {
+            val intent = Intent(this, ReservationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reservation_entrance)
+        binding.lifecycleOwner = this
     }
 }
