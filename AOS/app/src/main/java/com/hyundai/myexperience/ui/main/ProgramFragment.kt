101ed8c5c3df6a9b576b2583c4494463e65cb1f9
ThinkingDobby <chiho1221@naver.com>
2024-02-09 20:41:32 +0900

    [AOS] feature: 프로그램 정보 화면 구현 준비
    파일 생성 및 프로그램 관련 화면 사진 변경

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/main/ProgramFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/main/ProgramFragment.kt
@@ -1,18 +1,38 @@
 package com.hyundai.myexperience.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.FragmentProgramBinding
+import com.hyundai.myexperience.ui.program_info.ProgramCategoryActivity
 
 class ProgramFragment : Fragment() {
+    private var _binding: FragmentProgramBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_program, container, false)
+    ): View {
+        _binding = FragmentProgramBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivExperienceBackground.setOnClickListener {
+            val intent = Intent(requireActivity(), ProgramCategoryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramCategoryActivity.kt
@@ -0,0 +1,27 @@
+package com.hyundai.myexperience.ui.program_info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.ActivityProgramCategoryBinding
+import com.hyundai.myexperience.utils.navigationHeight
+import com.hyundai.myexperience.utils.setStatusBarTransparent
+
+class ProgramCategoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProgramCategoryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initDataBinding()
+
+        this.setStatusBarTransparent()
+        binding.cl.setPadding(0, 0, 0, navigationHeight())
+    }
+
+    private fun initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_program_category)
+        binding.lifecycleOwner = this
+    }
+}
