6f5b34aad3f7a43db36401bcdaca01327d0b8ef1
youngseo <youngseojang@gmail.com>
2024-02-10 20:37:57 +0900

    [AOS] feature: 로그인, 회원가입 페이지에 툴바 적용

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/signin/SignInActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/signin/SignInActivity.kt
@@ -1,18 +1,46 @@
 package com.hyundai.myexperience.ui.signin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.TextureView
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import com.hyundai.myexperience.R
 import com.hyundai.myexperience.databinding.ActivitySignInBinding
+import com.hyundai.myexperience.ui.signup.SignUpActivity
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
+    //private lateinit var toolbarBinding: ToolbarLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
         binding.lifecycleOwner = this
+
+        val intent = Intent(this, SignUpActivity::class.java)
+        binding.tvSignup.setOnClickListener{startActivity(intent)}
+
+        var toolbar = binding.toolbar as Toolbar
+        var toolbarTitle = toolbar.findViewById<TextView>(R.id.toolBar_title) // findViewById 안 쓰는 방법?
+        toolbarTitle.text = ""
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- AOS/app/src/main/java/com/hyundai/myexperience/ui/signup/SignUpActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/signup/SignUpActivity.kt
@@ -2,11 +2,36 @@ package com.hyundai.myexperience.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        binding.lifecycleOwner = this
+
+        var toolbar = binding.toolbar as Toolbar
+        var toolbarTitle = toolbar.findViewById<TextView>(R.id.toolBar_title) // findViewById 안 쓰는 방법?
+        toolbarTitle.text = ""
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //supportActionBar?.apply{
+        //    // 뒤로가기 버튼 활성화
+        //    setDisplayHomeAsUpEnabled(true)
+        //    setDisplayShowHomeEnabled(true)
+        //}
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
