5f614a354d2c290615a18ba6ff09d0e88055459e
ThinkingDobby <chiho1221@naver.com>
2024-02-10 16:56:13 +0900

    [AOS] feature: 교육 차량 이미지 슬라이더 적용

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramConfFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramConfFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hyundai.myexperience.R
 import com.hyundai.myexperience.databinding.FragmentProgramConfBinding
@@ -27,8 +30,11 @@ class ProgramConfFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val imageList = listOf(R.drawable.program_category_iv_background, R.drawable.main_iv_background)
-        val pagerImageAdapter = PagerImageAdapter(imageList)
-        binding.vpProgramImage.adapter = pagerImageAdapter
+        val programImageAdapter = PagerImageAdapter(imageList)
+        binding.vpProgramImage.adapter = programImageAdapter
+
+        val carImageAdapter = PagerImageAdapter(imageList)
+        binding.vpCarImage.adapter = carImageAdapter
 
         binding.btnProgramImagePrev.setOnClickListener {
             binding.vpProgramImage.currentItem = binding.vpProgramImage.currentItem - 1
@@ -38,12 +44,56 @@ class ProgramConfFragment : Fragment() {
             binding.vpProgramImage.currentItem = binding.vpProgramImage.currentItem + 1
         }
 
+        binding.btnCarImagePrev.setOnClickListener {
+            binding.vpCarImage.currentItem = binding.vpCarImage.currentItem - 1
+        }
+
+        binding.btnCarImageNext.setOnClickListener {
+            binding.vpCarImage.currentItem = binding.vpCarImage.currentItem + 1
+        }
+
         TabLayoutMediator(binding.tlProgramImage, binding.vpProgramImage) { _, _ ->
         }.attach()
+
+        initializeIndicators(imageList.size)
+
+        binding.vpCarImage.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                updateIndicators(position)
+            }
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun initializeIndicators(count: Int) {
+        binding.llCarImageIndicator.removeAllViews()
+
+        for (i in 0 until count) {
+            val imageView = ImageView(requireContext()).apply {
+                layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).also {
+                    it.setMargins(8, 0, 8, 0)
+                }
+                setImageResource(R.drawable.icon_circle_unfilled)
+            }
+            binding.llCarImageIndicator.addView(imageView)
+        }
+
+        if (count > 0) {
+            (binding.llCarImageIndicator.getChildAt(0) as ImageView).setImageResource(R.drawable.icon_circle_filled)
+        }
+    }
+
+    fun updateIndicators(position: Int) {
+        for (i in 0 until binding.llCarImageIndicator.childCount) {
+            (binding.llCarImageIndicator.getChildAt(i) as ImageView).setImageResource(
+                if (i == position) R.drawable.icon_circle_filled else R.drawable.icon_circle_unfilled
+            )
+        }
     }
 }
