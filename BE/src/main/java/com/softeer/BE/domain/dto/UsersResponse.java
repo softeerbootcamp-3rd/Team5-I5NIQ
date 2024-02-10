303eec8a8621514ae6332faf6ac72fc088fee40d
isExample <yeji0331@naver.com>
2024-02-10 14:30:23 +0900

    [BE] feature: 마이페이지 조회 ResponseDTO 구현
    마이페이지 화면을 조회하기 위한 응답 dto를 구현했습니다.

--- BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
+++ BE/src/main/java/com/softeer/BE/domain/dto/UsersResponse.java
@@ -1,6 +1,7 @@
 package com.softeer.BE.domain.dto;
 
 import com.softeer.BE.domain.entity.Car;
+import com.softeer.BE.domain.entity.Comment;
 import com.softeer.BE.domain.entity.Participation;
 import com.softeer.BE.domain.entity.Program;
 import lombok.AllArgsConstructor;
@@ -65,6 +66,69 @@ public class UsersResponse {
               car.getName(),
               participation.getParticipants(),
               startDateTime
+      );
+    }
+  }
+
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Getter
+  public static class UpcomingClass{
+    private int num;
+    private String category;
+    private String programName;
+    private String level;
+    private LocalDateTime startDateTime;
+
+    public static UpcomingClass of(int num, Program program, LocalDateTime startDateTime){
+      return new UpcomingClass(
+              num,
+              program.getCategory().name(),
+              program.getName().name(),
+              program.getLevel().name(),
+              startDateTime
+      );
+    }
+  }
+
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Getter
+  public static class RecentComment{
+    private String category;
+    private String programName;
+    private String level;
+    private String contents;
+
+    public static RecentComment of(Comment comment){
+      return new RecentComment(
+        comment.getProgram().getCategory().name(),
+        comment.getProgram().getName().name(),
+        comment.getProgram().getLevel().name(),
+        comment.getContent()
+      );
+    }
+  }
+
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Getter
+  public static class MyPageContents{
+    private String name;
+    private int level;
+    private int totalClassNum;
+    private int pastClassNum;
+    private UpcomingClass upcomingClass;
+    private RecentComment recentComment;
+
+    public static MyPageContents of(UpcomingClass upcomingClass, RecentComment recentComment, String name, int level, int totalNum, int pastNum){
+      return new MyPageContents(
+              name,
+              level,
+              totalNum,
+              pastNum,
+              upcomingClass,
+              recentComment
       );
     }
   }
