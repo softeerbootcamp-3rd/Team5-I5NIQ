eb5e3d5662c33e42908aa44a1f262aaadd92a213
ThinkingDobby <chiho1221@naver.com>
2024-02-10 13:02:12 +0900

    [AOS] feature: 프로그램 정보 Fragment 생성

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramInfoActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramInfoActivity.kt
@@ -3,8 +3,13 @@ package com.hyundai.myexperience.ui.program_info
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
 import com.hyundai.myexperience.R
 import com.hyundai.myexperience.databinding.ActivityProgramInfoBinding
+import com.hyundai.myexperience.ui.common.PagerAdapter
+import com.hyundai.myexperience.ui.reservation.ReservationSessionHeadCountFragment
+import com.hyundai.myexperience.ui.reservation.program_first.ReservationCarDateFragment
+import com.hyundai.myexperience.ui.reservation.program_first.ReservationProgramFragment
 import com.hyundai.myexperience.utils.navigationHeight
 import com.hyundai.myexperience.utils.setStatusBarTransparent
 
@@ -18,6 +23,18 @@ class ProgramInfoActivity : AppCompatActivity() {
 
         this.setStatusBarTransparent()
         binding.clMain.setPadding(0, 0, 0, navigationHeight())
+
+        val pagerAdapter = PagerAdapter(this)
+        pagerAdapter.addFragment(ProgramMajorFragment())
+        pagerAdapter.addFragment(ProgramConfFragment())
+        pagerAdapter.addFragment(ProgramTrackFragment())
+        pagerAdapter.addFragment(ProgramCommentFragment())
+
+        binding.vp.adapter = pagerAdapter
+
+        TabLayoutMediator(binding.tl, binding.vp) { tab, position ->
+            tab.text = resources.getStringArray(R.array.programTabTexts)[position]
+        }.attach()
     }
 
     private fun initDataBinding() {
