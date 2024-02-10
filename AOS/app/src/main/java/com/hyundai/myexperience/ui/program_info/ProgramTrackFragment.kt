673b9ab38c1467f075a8d4999c97c1dfc7b21f1a
ThinkingDobby <chiho1221@naver.com>
2024-02-10 23:33:20 +0900

    [AOS] feature: ProgramTrackFragment 디자인 적용

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramTrackFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/program_info/ProgramTrackFragment.kt
@@ -6,15 +6,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.FragmentProgramTrackBinding
+import com.hyundai.myexperience.ui.common.PagerImageAdapter
 
 class ProgramTrackFragment : Fragment() {
+    private var _binding: FragmentProgramTrackBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_program_track, container, false)
+    ): View {
+        _binding = FragmentProgramTrackBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = listOf(R.drawable.program_category_iv_background, R.drawable.main_iv_background)
+        val mapImageAdapter = PagerImageAdapter(imageList)
+        binding.vpMapImage.adapter = mapImageAdapter
+
+        binding.btnMapImagePrev.setOnClickListener {
+            binding.vpMapImage.currentItem = binding.vpMapImage.currentItem - 1
+        }
+
+        binding.btnMapImageNext.setOnClickListener {
+            binding.vpMapImage.currentItem = binding.vpMapImage.currentItem + 1
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
