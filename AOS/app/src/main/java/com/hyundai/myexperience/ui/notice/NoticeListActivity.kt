497d04832fedf356c27e827502f304080b3edd97
youngseo <youngseojang@gmail.com>
2024-02-11 22:11:18 +0900

    [AOS] feature: 기본 레이아웃 구성 툴바, 리사이클러뷰 아이템 구성

---
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/notice/NoticeListActivity.kt
@@ -0,0 +1,12 @@
+package com.hyundai.myexperience.ui.notice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyundai.myexperience.R
+
+class NoticeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notice_list)
+    }
+}
