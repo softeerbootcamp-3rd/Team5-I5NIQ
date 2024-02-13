dc1facb9e7b25b7a381bb90c07b1251c5f2dabfc
isExample <yeji0331@naver.com>
2024-02-13 23:16:20 +0900

    [BE] feature: 예약 차량 먼저 선택하기 Step1 controller 구현
    예약 차량 먼저 선택하기 Step1에 대한 controller를 구현했습니다.
    로그인한 사용자만이 호출할 수 있도록 했습니다.

---
+++ BE/src/main/java/com/softeer/BE/controller/ReservationController.java
@@ -0,0 +1,29 @@
+package com.softeer.BE.controller;
+
+import com.softeer.BE.domain.dto.ReservationResponse;
+import com.softeer.BE.global.apiPayload.ApiResponse;
+import com.softeer.BE.global.apiPayload.code.statusEnums.ErrorStatus;
+import com.softeer.BE.global.exception.GeneralHandler;
+import com.softeer.BE.service.ReservationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservation")
+public class ReservationController {
+    private final ReservationService reservationService;
+    @GetMapping("/step1/car")
+    public ApiResponse<List<ReservationResponse.Step1CarStatus>> listStep1CarStatus(HttpServletRequest request){
+        HttpSession httpSession = request.getSession(false);
+        if(httpSession == null)
+            throw new GeneralHandler(ErrorStatus._UNAUTHORIZED);
+        return ApiResponse.onSuccess((reservationService.getStep1CarStatusList()));
+    }
+}
