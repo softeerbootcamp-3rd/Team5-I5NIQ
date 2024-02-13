ce61d3b52f6375524cecfcfa0d5c79f88a5c20c7
johan1103 <johan1103@naver.com>
2024-02-13 18:18:45 +0900

    [BE] feature: step2 용 controller 구현
    controller 용도 설명하는 주석 추가

--- BE/src/main/java/com/softeer/BE/controller/ReservationController.java
+++ BE/src/main/java/com/softeer/BE/controller/ReservationController.java
@@ -1,11 +1,13 @@
 package com.softeer.BE.controller;
 
+import com.softeer.BE.domain.dto.ReservationResponse.DateCarSelectMenu;
 import com.softeer.BE.domain.dto.ReservationResponse.ProgramSelectMenu;
 import com.softeer.BE.global.apiPayload.ApiResponse;
 import com.softeer.BE.service.ProgramReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/reservation")
 public class ReservationController {
   private final ProgramReservationService programReservationService;
+  //program id 결정
   @GetMapping("/step1/program")
   public ApiResponse<ProgramSelectMenu> getAllProgramReservationStatus(){
     ProgramSelectMenu result = programReservationService.searchForAvailableProgram();
     return ApiResponse.onSuccess(result);
+  }
+  //날짜(시간 제외)결정, 차량 결정
+  @GetMapping("/step2/program")
+  public ApiResponse<DateCarSelectMenu> getAllDateCarSelectMenu(@RequestParam(value = "program-id")Long programId){
+    return ApiResponse.onSuccess(programReservationService.searchForAvailableDateCars(programId));
   }
 }
