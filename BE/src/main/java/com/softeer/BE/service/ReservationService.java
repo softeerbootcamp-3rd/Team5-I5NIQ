f4af8378f87e67e0cae5d5a309d3ea3d2562c589
isExample <yeji0331@naver.com>
2024-02-13 23:17:24 +0900

    [BE] feature: 예약 차량 먼저 선택하기 Step1 service 구현
    ClassCar 목록을 조회하고 각 ClassCar의 예약 가능 여부를 응답 DTO로 변환하여 반환합니다.

---
+++ BE/src/main/java/com/softeer/BE/service/ReservationService.java
@@ -0,0 +1,35 @@
+package com.softeer.BE.service;
+
+import com.softeer.BE.domain.dto.ReservationResponse;
+import com.softeer.BE.domain.entity.ClassCar;
+import com.softeer.BE.domain.entity.Participation;
+import com.softeer.BE.repository.ClassCarRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReservationService {
+    private final ClassCarRepository classCarRepository;
+
+    public List<ReservationResponse.Step1CarStatus> getStep1CarStatusList(){
+        // 현재 일시가 예약 시작 시간과 예약 마감 시간 사이인 ClassCar 목록을 조회한다.
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        List<ClassCar> availableClassCars = classCarRepository.findAvailableClassCars(currentDateTime);
+
+        // 각 ClassCar에 대해 maximum_occupancy > participants 합 결과를 저장한다.
+        return availableClassCars.stream().map(classCar -> {
+            long totalParticipants = classCar.getParticipationList().stream()
+                    .mapToLong(Participation::getParticipants)
+                    .sum();
+            boolean isAvailable = totalParticipants < classCar.getDrivingClass().getProgram().getMaximumOccupancy();
+            return ReservationResponse.Step1CarStatus.of(classCar.getCar().getName(), isAvailable);
+        }).collect(Collectors.toList());
+    }
+}
