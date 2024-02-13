3475e8270bf06f61d3f84dcc60473b8428f68531
youngseo <youngseojang@gmail.com>
2024-02-13 18:42:23 +0900

    [AOS] feature: 참여 예정 프로그램 리사이클러뷰로 구현

---
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/joined_program/JoinedProgramActivity.kt
@@ -0,0 +1,55 @@
+package com.hyundai.myexperience.ui.joined_program
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.ActivityJoinedProgramBinding
+import com.hyundai.myexperience.ui.joined_program.adapter.ProgramsAdapter
+
+class JoinedProgramActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJoinedProgramBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initDataBinding()
+
+        val toolbarLayout = binding.toolbarLayout
+        toolbarLayout.toolBarTitle.text = "참여 예정 프로그램"
+        setSupportActionBar(toolbarLayout.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        var programsList = listOf(
+            ProgramsItem("24년 2월 3일 오후 3시", "제네시스 드라이빙 익스피리언스 Level2"),
+            ProgramsItem("24년 2월 2일 오후 3시", "현대 드라이빙 익스피리언스 Level2"),
+            ProgramsItem("24년 2월 1일 오후 3시", "현대 드라이빙 익스피리언스 Level1")
+        )
+
+        val adapter = ProgramsAdapter(programsList)
+        binding.rvJoinedPrograms.adapter = adapter
+        binding.rvJoinedPrograms.layoutManager = LinearLayoutManager(this)
+
+
+    }
+
+    private fun initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_joined_program)
+        binding.lifecycleOwner = this
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/joined_program/adapter/ProgramsAdapter.kt
@@ -0,0 +1,33 @@
+package com.hyundai.myexperience.ui.joined_program.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hyundai.myexperience.databinding.ItemJoinedProgramBinding
+import com.hyundai.myexperience.ui.joined_program.ProgramsItem
+
+class ProgramsAdapter (
+        private var programs : List<ProgramsItem>,
+    ):
+        RecyclerView.Adapter<ProgramViewHolder>() {
+
+    private val onItemClickListener: ProgramsItemClickListener = object :
+        ProgramsItemClickListener {
+        override fun onItemClick(program: ProgramsItem) {
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgramViewHolder {
+        val itemBinding: ItemJoinedProgramBinding =
+            ItemJoinedProgramBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProgramViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ProgramViewHolder, position: Int) {
+        holder.bind(programs[position], onItemClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return programs.size
+    }
+}
