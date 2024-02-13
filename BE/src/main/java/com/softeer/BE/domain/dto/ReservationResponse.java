a869d3d55abd7076ae53a414e0b47112c2563ce4
isExample <yeji0331@naver.com>
2024-02-14 01:47:38 +0900

    [BE] feature: 예약 차량 먼저 선택하기 Step2 응답 DTO 구현
    프로그램 이름 - 카테고리, 레벨 - 일시, 상태를 나타내는 응답 DTO를 구현했습니다.

--- BE/src/main/java/com/softeer/BE/domain/dto/ReservationResponse.java
+++ BE/src/main/java/com/softeer/BE/domain/dto/ReservationResponse.java
@@ -1,9 +1,12 @@
 package com.softeer.BE.domain.dto;
 
 import com.softeer.BE.domain.entity.Car;
+import com.softeer.BE.domain.entity.Program;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 public class ReservationResponse {
     @AllArgsConstructor
@@ -18,6 +21,52 @@ public class ReservationResponse {
                     car.getId(),
                     car.getName(),
                     isAvailable
+            );
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class ClassDetail{
+        private String monthDate;
+        private String status;
+        public static ClassDetail of(String monthDate, String status){
+            return new ClassDetail(
+                    monthDate,
+                    status
+            );
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class ProgramDetail{
+        private String category;
+        private String level;
+        private List<ClassDetail> classDetailList;
+        public static ProgramDetail of(Program program, List<ClassDetail> classDetailList){
+            return new ProgramDetail(
+                    program.getCategory().name(),
+                    program.getLevel().name(),
+                    classDetailList
+            );
+        }
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    public static class Step2ProgramStatus{
+        private Long programId;
+        private String programName;
+        private List<ProgramDetail> programDetailList;
+        public static Step2ProgramStatus of(Program program, List<ProgramDetail> programDetailList){
+            return new Step2ProgramStatus(
+                    program.getId(),
+                    program.getName().name(),
+                    programDetailList
             );
         }
     }
