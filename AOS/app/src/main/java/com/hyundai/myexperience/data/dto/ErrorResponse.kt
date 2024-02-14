a184ddca7eec3fdd666eca623507943e89bacbec
ThinkingDobby <chiho1221@naver.com>
2024-02-15 00:55:53 +0900

    [AOS] feature: 로그인 통신 오류 대응 추가
    통신 확인, 오류 응답 매핑 추가

--- AOS/app/src/main/java/com/hyundai/myexperience/data/dto/ErrorResponse.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/data/dto/ErrorResponse.kt
@@ -3,6 +3,9 @@ package com.hyundai.myexperience.data.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LoginResponse(
+data class ErrorResponse (
+    val code: String,
+    val isSuccess: Boolean,
     val message: String,
+    val result: String
 )
