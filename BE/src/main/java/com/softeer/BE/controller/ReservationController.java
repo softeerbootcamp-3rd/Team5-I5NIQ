ed7a22bb777b959438d2b8c21688d644661bd52c
KTH <johan1103@naver.com>
2024-02-14 19:45:03 +0900

    [BE] feature: 예약 담당하는 controller 구현, 인증/인가 코드 미적용
    다른 step1,2 코드들과 함께 인증 로직 적용 예정

--- BE/src/main/java/com/softeer/BE/controller/ReservationController.java
+++ BE/src/main/java/com/softeer/BE/controller/ReservationController.java
@@ -1,14 +1,12 @@
 package com.softeer.BE.controller;
 
-import com.softeer.BE.domain.dto.ReservationStep3Response;
 import com.softeer.BE.domain.dto.ReservationStep3Response.ProgramSelectMenuStep3;
+import com.softeer.BE.domain.entity.Users;
 import com.softeer.BE.global.apiPayload.ApiResponse;
+import com.softeer.BE.repository.UsersRepository;
 import com.softeer.BE.service.ReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -17,10 +15,20 @@ import java.time.LocalDate;
 @RequestMapping("/reservation")
 public class ReservationController {
   private final ReservationService reservationService;
+  private final UsersRepository usersRepository;
   @GetMapping("/step3")
   public ApiResponse<ProgramSelectMenuStep3> getAllSelectMenuInStep3(@RequestParam(value = "program-id")Long programId
           ,@RequestParam(value = "car-id")Long carId
           ,@RequestParam(value = "reservation-date")LocalDate reservationDate){
    return ApiResponse.onSuccess(reservationService.searchForStep3AvailableClassCar(reservationDate,programId,carId));
+  }
+
+  @PostMapping
+  public ApiResponse<Boolean> reservation(@RequestParam("class-id")long classCarId,
+                                          @RequestParam("amount")long reservationSize){
+    Users user = usersRepository.findById("userId1").get();
+    if(reservationSize<1)
+      ApiResponse.onFailure("invalid param","reservationSize값은 1이상이어야 합니다.",false);
+    return ApiResponse.onSuccess(reservationService.classCarReservation(classCarId,reservationSize,user));
   }
 }
