cdec16a00b808306b8a836299ab44a95738adc48
KTH <johan1103@naver.com>
2024-02-14 13:39:37 +0900

    [BE] feature: 특정 날짜,프로그램,자동차 정보랑 일치하는 예약 정보 반환 쿼리 구현

---
+++ BE/src/main/java/com/softeer/BE/repository/ClassCarRepository.java
@@ -0,0 +1,24 @@
+package com.softeer.BE.repository;
+
+import com.softeer.BE.domain.entity.ClassCar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ClassCarRepository extends JpaRepository<ClassCar,Long> {
+  @Query("SELECT cc FROM class_car cc JOIN cc.drivingClass dc JOIN dc.program p JOIN cc.car c " +
+          "WHERE DAY(:reservation_date) = DAY(dc.startDateTime) AND " +
+          "MONTH(:reservation_date) = MONTH(dc.startDateTime) AND " +
+          "YEAR(:reservation_date) = YEAR(dc.startDateTime) AND " +
+          "p.id=:program_id AND " +
+          "c.id=:car_id AND " +
+          ":today BETWEEN dc.reservationStartTime AND dc.reservationDeadline")
+  List<ClassCar> findAllByStep2(@Param(value = "reservation_date")LocalDate reservationDate,
+                                @Param(value = "program_id")long programId,
+                                @Param(value = "car_id")long carId,
+                                @Param(value = "today")LocalDateTime today);
+}
