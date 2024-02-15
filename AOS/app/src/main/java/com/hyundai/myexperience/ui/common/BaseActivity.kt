0d1312f7d96dd2569ece6dc86f38bef2fc9b4953
ThinkingDobby <chiho1221@naver.com>
2024-02-15 11:15:48 +0900

    [AOS] feature: BaseActivity 화면 전환 애니메이션 추가

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/common/BaseActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/common/BaseActivity.kt
@@ -1,11 +1,36 @@
 package com.hyundai.myexperience.ui.common
 
+import android.os.Build
+import android.os.Bundle
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.hyundai.myexperience.R
 
 open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTransitionOnCreate()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun finish() {
+        super.finish()
+
+        setTransitionOnFinish()
+    }
 
     protected fun setToolbar(toolbar: Toolbar, titleTv: TextView, title: String) {
         titleTv.text = title
@@ -17,13 +42,18 @@ open class BaseActivity : AppCompatActivity() {
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                onBackPressedDispatcher.onBackPressed()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+    private fun setTransitionOnCreate() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+        } else {
+            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, R.anim.fade_in, R.anim.fade_out)
+            overrideActivityTransition(OVERRIDE_TRANSITION_CLOSE, R.anim.fade_in, R.anim.fade_out)
+        }
+    }
+
+    private fun setTransitionOnFinish() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         }
     }
 }
