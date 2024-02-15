f3112583f9452eefc92ace15e5540dd5eb49bcaa
ThinkingDobby <chiho1221@naver.com>
2024-02-16 08:55:07 +0900

    [AOS] feature: 회차 및 인원 화면 버튼 기능 구현

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationSessionHeadCountFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationSessionHeadCountFragment.kt
@@ -2,6 +2,7 @@ package com.hyundai.myexperience.ui.reservation
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,10 +13,11 @@ import com.hyundai.myexperience.R
 import com.hyundai.myexperience.RESERVATION_STATUS_ABLE
 import com.hyundai.myexperience.RESERVATION_STATUS_SOLDOUT
 import com.hyundai.myexperience.data.entity.ReservationDate
-import com.hyundai.myexperience.databinding.FragmentReservationCarDateBinding
 import com.hyundai.myexperience.databinding.FragmentReservationSessionHeadcountBinding
 import com.hyundai.myexperience.ui.reservation.adapter.ReservationDateAdapter
 import com.hyundai.myexperience.ui.reservation.listener.DateClickListener
+import kotlin.math.max
+import kotlin.math.min
 
 class ReservationSessionHeadCountFragment : Fragment() {
     private var _binding: FragmentReservationSessionHeadcountBinding? = null
@@ -34,6 +36,33 @@ class ReservationSessionHeadCountFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initCardViews()
+
+        val editableFactory = Editable.Factory.getInstance()
+
+        binding.btnMinus.setOnClickListener {
+            val cnt = binding.etHeadcount.text.toString().toInt()
+            binding.etHeadcount.text = editableFactory.newEditable(max(1, cnt - 1).toString())
+        }
+
+        binding.btnPlus.setOnClickListener {
+            val cnt = binding.etHeadcount.text.toString().toInt()
+            binding.etHeadcount.text = editableFactory.newEditable(min(9, cnt + 1).toString())
+        }
+
+        var participation = true
+        binding.btnParticipation.setOnClickListener {
+            if (!participation) {
+                participation = true
+                setParticipationBtnColor(true)
+            }
+        }
+
+        binding.btnNonParticipation.setOnClickListener {
+            if (participation) {
+                participation = false
+                setParticipationBtnColor(false)
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -80,6 +109,25 @@ class ReservationSessionHeadCountFragment : Fragment() {
             }
 
             clicked[0] = !clicked[0]
+        }
+    }
+
+    private fun setParticipationBtnColor(participation: Boolean) {
+        val white = ContextCompat.getColor(requireContext(), R.color.white)
+        val gray2 = ContextCompat.getColor(requireContext(), R.color.gray2)
+
+        if (participation) {
+            binding.btnParticipation.strokeColor = ColorStateList.valueOf(white)
+            binding.btnParticipation.setTextColor(white)
+
+            binding.btnNonParticipation.strokeColor = ColorStateList.valueOf(gray2)
+            binding.btnNonParticipation.setTextColor(gray2)
+        } else {
+            binding.btnParticipation.strokeColor = ColorStateList.valueOf(gray2)
+            binding.btnParticipation.setTextColor(gray2)
+
+            binding.btnNonParticipation.strokeColor = ColorStateList.valueOf(white)
+            binding.btnNonParticipation.setTextColor(white)
         }
     }
 }
