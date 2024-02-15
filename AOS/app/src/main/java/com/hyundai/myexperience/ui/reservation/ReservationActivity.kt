2cf7afc58197adfbc8016f91d7e7939cf7abf55e
ThinkingDobby <chiho1221@naver.com>
2024-02-15 13:55:38 +0900

    [AOS] feature: 예약 타입별 Fragment 적용 구현

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/ReservationActivity.kt
@@ -1,15 +1,21 @@
 package com.hyundai.myexperience.ui.reservation
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.RESERVATION_CAR_FIRST
+import com.hyundai.myexperience.RESERVATION_DATE_FIRST
+import com.hyundai.myexperience.RESERVATION_PROGRAM_FIRST
+import com.hyundai.myexperience.RESERVATION_TYPE_KEY
 import com.hyundai.myexperience.databinding.ActivityReservationBinding
 import com.hyundai.myexperience.ui.common.BaseActivity
 import com.hyundai.myexperience.ui.common.PagerFragmentAdapter
+import com.hyundai.myexperience.ui.reservation.car_or_date_first.ReservationCarFragment
+import com.hyundai.myexperience.ui.reservation.car_or_date_first.ReservationDateProgramFragment
 import com.hyundai.myexperience.ui.reservation.program_first.ReservationCarDateFragment
 import com.hyundai.myexperience.ui.reservation.program_first.ReservationProgramFragment
 import com.hyundai.myexperience.utils.navigationHeight
@@ -24,7 +30,8 @@ class ReservationActivity : BaseActivity() {
         initDataBinding()
         initScreen()
 
-        initPager()
+        val type = intent.getIntExtra(RESERVATION_TYPE_KEY, -1)
+        initPager(type)
 
         binding.btnNext.setOnClickListener {
             binding.fcv.visibility = View.VISIBLE
@@ -42,17 +49,53 @@ class ReservationActivity : BaseActivity() {
         this.setStatusBarTransparent()
         binding.cl.setPadding(0, 0, 0, this.navigationHeight())
 
-        setToolbar(binding.toolbarLayout.toolbar, binding.toolbarLayout.toolBarTitle, resources.getString(R.string.main_reservation))
+        setToolbar(
+            binding.toolbarLayout.toolbar,
+            binding.toolbarLayout.toolBarTitle,
+            resources.getString(R.string.main_reservation)
+        )
     }
 
-    private fun initPager() {
+    private fun initPager(type: Int) {
         val pagerFragmentAdapter = PagerFragmentAdapter(this)
-        pagerFragmentAdapter.addFragment(ReservationProgramFragment())
-        pagerFragmentAdapter.addFragment(ReservationCarDateFragment())
+
+        val fragments = getFragmentsByType(type)
+        for (fragment in fragments) {
+            pagerFragmentAdapter.addFragment(fragment)
+        }
         pagerFragmentAdapter.addFragment(ReservationSessionHeadCountFragment())
 
         binding.vp.adapter = pagerFragmentAdapter
+        binding.vp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tvStep.text = getString(R.string.reservation_step, position + 1)
+                if (position >= 2) binding.vPriceBackground.visibility = View.VISIBLE
+            }
+        })
 
         TabLayoutMediator(binding.tl, binding.vp) { _, _ -> }.attach()
     }
+
+    private fun getFragmentsByType(type: Int): List<Fragment> {
+        return when (type) {
+            RESERVATION_PROGRAM_FIRST -> listOf(
+                ReservationProgramFragment(),
+                ReservationCarDateFragment()
+            )
+
+            RESERVATION_DATE_FIRST -> listOf(
+                ReservationCarFragment(),
+                ReservationDateProgramFragment()
+            )
+
+            RESERVATION_CAR_FIRST -> listOf(
+                ReservationDateProgramFragment(),
+                ReservationCarFragment()
+            )
+
+            else -> listOf()
+        }
+    }
+
 }
