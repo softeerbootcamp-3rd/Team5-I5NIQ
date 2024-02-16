36d2a78e7fa8ac828c62f3d9a4343d7ec8def78d
isExample <yeji0331@naver.com>
2024-02-16 11:03:34 +0900

    [BE] fix: 참여한 예약 수업 판단 기준 수정에 따른 query문 수정
    수업의 시작 일시 필드 기준 -> 참여 여부 필드 기준을 수정했습니다.

--- BE/src/main/java/com/softeer/BE/repository/ParticipationRepository.java
+++ BE/src/main/java/com/softeer/BE/repository/ParticipationRepository.java
@@ -13,7 +13,7 @@ public interface ParticipationRepository extends JpaRepository<Participation, Lo
     @Query("SELECT p FROM participation p WHERE p.user.id = :userId AND p.classCar.drivingClass.startDateTime > CURRENT_TIMESTAMP")
     List<Participation> findUpcomingParticipationByUserId(@Param("userId") String userId);
 
-    @Query("SELECT p FROM participation p WHERE p.user.id = :userId AND p.classCar.drivingClass.startDateTime <= CURRENT_TIMESTAMP")
+    @Query("SELECT p FROM participation p WHERE p.user.id = :userId AND p.completion = true")
     List<Participation> findPastParticipationByUserId(@Param("userId") String userId);
 
     @Query("SELECT p FROM participation p WHERE p.user.id = :userId ORDER BY p.classCar.drivingClass.startDateTime ASC")
