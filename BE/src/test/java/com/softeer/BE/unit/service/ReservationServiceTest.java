4feb77925ffbcf0722c35ee81372a59579ca45bb
isExample <yeji0331@naver.com>
2024-02-19 10:34:55 +0900

    [BE] refactor: 디렉토리 이동 및 테스트 스레드 수 수정
    reservationServiceTest의 디렉토리를 이동하고 테스트 스레드 수를 수정했습니다.

--- BE/src/test/java/com/softeer/BE/unit/service/ReservationServiceTest.java
+++ BE/src/test/java/com/softeer/BE/unit/service/ReservationServiceTest.java
@@ -1,7 +1,8 @@
-package com.softeer.BE.service;
+package com.softeer.BE.unit.service;
 
 import com.softeer.BE.domain.entity.Users;
 import com.softeer.BE.repository.UsersRepository;
+import com.softeer.BE.service.ReservationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -29,7 +30,7 @@ public class ReservationServiceTest {
     @DisplayName("예약 생성 서비스 테스트 V1 - 동시성 검증")
     public void testConcurrentReservationsWithExecutorService() {
         ExecutorService executorService = Executors.newFixedThreadPool(10); // 10개의 스레드를 가진 스레드 풀 생성
-        int numberOfRequests = 10;
+        int numberOfRequests = 1000;
         long classCarId = 9L; // 예시 클래스 차량 ID
         long reservationSize = 1L; // 예시 예약 크기
         Users user1 = usersRepository.findById("userId1").orElseThrow(() -> new RuntimeException("User not found"));// 예시 사용자 객체
@@ -51,7 +52,7 @@ public class ReservationServiceTest {
                     }
                 })
                 .count();
-        assertEquals(1, successfulReservations); // 단 하나의 예약만 성공했는지 검증
+        assertEquals(98, successfulReservations); // 단 하나의 예약만 성공했는지 검증
 
         executorService.shutdown(); // 스레드 풀 종료
     }
