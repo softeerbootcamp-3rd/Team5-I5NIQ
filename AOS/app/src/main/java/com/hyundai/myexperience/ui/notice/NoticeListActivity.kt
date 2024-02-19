718aad6639a2505b71c5a2d24925b9811a1f86ca
youngseo <youngseojang@gmail.com>
2024-02-19 18:42:28 +0900

    [AOS] feature: 액티비티에서 서버에서 공지사항 목록 불러오기

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/notice/NoticeListActivity.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/notice/NoticeListActivity.kt
@@ -9,11 +9,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hyundai.myexperience.R
+import com.hyundai.myexperience.data.entity.NoticesItem
 import com.hyundai.myexperience.databinding.ActivityNoticeListBinding
 import com.hyundai.myexperience.ui.notice.adapter.NoticesAdapter
 import com.hyundai.myexperience.utils.navigationHeight
 import com.hyundai.myexperience.utils.setStatusBarTransparent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NoticeListActivity : AppCompatActivity() {
     private val viewModel: NoticeListViewModel by viewModels()
     private lateinit var binding: ActivityNoticeListBinding
@@ -34,27 +37,22 @@ class NoticeListActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
 
-        val noticeList = listOf(
-            NoticesItem("2024년 1월 1일", "HMG 드라이빙 익스피리언스 2024년 준비 기간 안내"),
-            NoticesItem("2023년 10월 10일", "2023년 HMG 드라이빙 익스피리언스 11월 운영 공지"),
-            NoticesItem("2023년 9월 10일", "2023년 HMG 드라이빙 익스피리언스 10월 운영 공지"),
-            NoticesItem("2023년 8월 10일", "2023년 HMG 드라이빙 익스피리언스 9월 운영 공지"),
-            NoticesItem("2023년 7월 10일", "2023년 HMG 드라이빙 익스피리언스 8월 운영 공지"),
-            NoticesItem("2023년 6월 10일", "2023년 HMG 드라이빙 익스피리언스 7월 운영 공지"),
-            NoticesItem("2023년 5월 10일", "2023년 HMG 드라이빙 익스피리언스 6월 운영 공지"),
-            NoticesItem("2023년 4월 10일", "2023년 HMG 드라이빙 익스피리언스 5월 운영 공지"),
-            NoticesItem("2023년 3월 10일", "2023년 HMG 드라이빙 익스피리언스 4월 운영 공지")
-        )
+        val adapter = NoticesAdapter(viewModel, viewModel.notices.value!!)
+        viewModel.getResponse()
 
-        val adapter = NoticesAdapter(viewModel, noticeList)
         binding.rvNotice.adapter = adapter
         binding.rvNotice.layoutManager = LinearLayoutManager(this)
+
+        viewModel.notices.observe(this, {
+            adapter.setData(it)
+        })
 
         viewModel.notice.observe(this, Observer { notice ->
             notice?.let {
                 val intent = Intent(this, NoticeDetailActivity::class.java)
                 intent.putExtra("title", notice.noticeTitle)
                 intent.putExtra("date", notice.date)
+                intent.putExtra("detail", notice.noticeDetail)
                 startActivity(intent)
             }
         })
