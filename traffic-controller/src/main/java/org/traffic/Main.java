8fc679f4fb682511a8180cc500fa5338ec1b193e
johan1103 <johan1103@naver.com>
2024-02-19 21:41:32 +0900

    [BE-RATE] feature: Listner용 스레드, Server용 스레드 분리해서 실행하도록 구현
    main thread는 Server.class가 사용하도록 구현

--- traffic-controller/src/main/java/org/traffic/Main.java
+++ traffic-controller/src/main/java/org/traffic/Main.java
@@ -1,14 +1,74 @@
 package org.traffic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.traffic.nio.Listener;
 import org.traffic.nio.Server;
+import org.traffic.nio.SocketChannelQueue;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   private final static int PORT=8080;
-  public static void main(String[] args) throws IOException {
+  private static Logger logger = LoggerFactory.getLogger(Main.class);
+  public static void main(String[] args) throws IOException, InterruptedException {
+    SocketChannelQueue socketChannelQueue = new SocketChannelQueue();
     System.out.println("Hello world!");
-    Server server = new Server();
-    server.start();
+    Thread thread = new Thread(()->{
+      try {
+      Server server = new Server(socketChannelQueue);
+      server.start();
+      }catch (Exception e){
+      }
+    });
+    thread.start();
+    Listener listener = new Listener(socketChannelQueue);
+    logger.info("listener start");
+    for(int i=0;i<100;i++){
+      Thread.sleep(10000);
+      listener.accessAllow(0);
+    }
+  }
+
+  public static void listenServiceServer(SocketChannelQueue socketChannelQueue) throws IOException {
+    Listener listener = new Listener(socketChannelQueue);
+    int port = 8080;
+    URL url = new URL("http://127.0.0.1:8080/i5niq");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+
+    int status = connection.getResponseCode();
+    BufferedReader in = new BufferedReader(
+            new InputStreamReader(connection.getInputStream()));
+    String inputLine;
+    StringBuffer content = new StringBuffer();
+    while ((inputLine = in.readLine()) != null) {
+      content.append(inputLine);
+    }
+    logger.info("served value : {}",content);
+    in.close();
+    connection.disconnect();
+  }
+
+  private static class ParameterStringBuilder {
+    public static String getParamsString(Map<String, String> params)
+            throws UnsupportedEncodingException {
+      StringBuilder result = new StringBuilder();
+
+      for (Map.Entry<String, String> entry : params.entrySet()) {
+        result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+        result.append("=");
+        result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        result.append("&");
+      }
+
+      String resultString = result.toString();
+      return resultString.length() > 0
+              ? resultString.substring(0, resultString.length() - 1)
+              : resultString;
+    }
   }
 }
