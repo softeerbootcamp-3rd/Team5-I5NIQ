5a27599f72a68d082a665922d4c8fad14553acf2
ThinkingDobby <chiho1221@naver.com>
2024-02-20 16:51:31 +0900

    [AOS] feature: 예약 상태 색상 적용

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/adapter/LevelViewHolder.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/adapter/LevelViewHolder.kt
@@ -1,6 +1,9 @@
 package com.hyundai.myexperience.ui.reservation.adapter
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.hyundai.myexperience.R
+import com.hyundai.myexperience.RESERVATION_STATUS_UNABLE
 import com.hyundai.myexperience.data.entity.Level
 import com.hyundai.myexperience.databinding.ItemProgramLevelBinding
 import com.hyundai.myexperience.ui.reservation.listener.LevelClickListener
@@ -8,11 +11,20 @@ import com.hyundai.myexperience.ui.reservation.listener.LevelClickListener
 class LevelViewHolder(private val binding: ItemProgramLevelBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(level: Level, levelClickListener: LevelClickListener) {
-        binding.clItem.setOnClickListener {
-            levelClickListener.onLevelClick(level.level)
-        }
-
         binding.tvLevel.text = level.level
         binding.tvReservationStatus.text = level.status
+
+        if (binding.tvReservationStatus.text == RESERVATION_STATUS_UNABLE) {
+            binding.tvLevel.setTextColor(
+                ContextCompat.getColor(binding.root.context, R.color.gray2)
+            )
+            binding.tvReservationStatus.setTextColor(
+                ContextCompat.getColor(binding.root.context, R.color.gray2)
+            )
+        } else {
+            binding.clItem.setOnClickListener {
+                levelClickListener.onLevelClick(level.level)
+            }
+        }
     }
 }
