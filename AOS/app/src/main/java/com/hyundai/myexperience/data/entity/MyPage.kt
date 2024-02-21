af44a066141f2a7e175181aaa460b570503429a5
youngseo <youngseojang@gmail.com>
2024-02-22 07:36:34 +0900

    [AOS] feature: 마이페이지 엔티티, 응답 정의

---
+++ AOS/app/src/main/java/com/hyundai/myexperience/data/entity/MyPage.kt
@@ -0,0 +1,10 @@
+package com.hyundai.myexperience.data.entity
+
+class MyPage (
+    val level: Int,
+    val name: String,
+    val pastClassNum: Int,
+    val recentComment: String?, // Any
+    val totalClassNum: Int,
+    val upcomingClass: String?  // Any
+)
