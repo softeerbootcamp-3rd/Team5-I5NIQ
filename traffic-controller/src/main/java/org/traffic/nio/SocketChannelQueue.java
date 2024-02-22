0aca0c1027ed14f7dd65bd7fe5389c90b3611ec4
KTH <johan1103@naver.com>
2024-02-22 22:23:57 +0900

    [BE-RATE] refactor: 복잡한 로깅 제거

--- traffic-controller/src/main/java/org/traffic/nio/SocketChannelQueue.java
+++ traffic-controller/src/main/java/org/traffic/nio/SocketChannelQueue.java
@@ -8,12 +8,13 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @NoArgsConstructor
 public class SocketChannelQueue {
   private final Logger logger = LoggerFactory.getLogger(SocketChannelQueue.class);
-  private final Queue<UserSocketChannel> socketChannels = new LinkedList<>();
+  private final ConcurrentLinkedQueue<UserSocketChannel> socketChannels = new ConcurrentLinkedQueue<>();
   private final AtomicInteger broadcastingSizeIdx = new AtomicInteger(0);
   private final int[] broadCastingSize = {200,400,800,1600,3200,6400,12800,25600,51200};
   public UserSocketChannel addChannel(SocketChannel socketChannel){
@@ -32,17 +33,13 @@ public class SocketChannelQueue {
   public void broadCast(ByteBuffer buffer){
     int broadcastingSizeLength = broadCastingSize.length;
     int loopCnt = broadCastingSize[broadcastingSizeIdx.incrementAndGet()%broadcastingSizeLength];
-    logger.info("--------------broadCast from Listener------------------");
-    logger.info("--SocketChannelQueue size : {}",socketChannels.size());
-    logger.info("--broadCast size : {}",loopCnt);
     int sequence=0;
     for (UserSocketChannel socketChannel : socketChannels) {
       //비동기로 socket에 write할 수 있어야 함.
-      socketChannel.renewPriority(buffer,++sequence);
-      if(sequence>=loopCnt)
+      socketChannel.renewPriority(buffer, ++sequence);
+      if (sequence >= loopCnt)
         break;
     }
-    logger.info("--------------end of Listener broadcast------------------");
   }
   public void removeInvalidSocketChannel(UserSocketChannel socketChannel){
     socketChannels.remove(socketChannel);
