6440144b2f0267d75a34b7ef16ce93d3b824ac42
youngseo <youngseojang@gmail.com>
2024-02-24 10:45:57 +0900

    [AOS] feature: 마이페이지 레벨 바인딩

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/main/my_page/MyPageBindingAdapter.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/main/my_page/MyPageBindingAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.hyundai.myexperience.R
 import com.hyundai.myexperience.RECENT_COMMENT_IS_NULL
 import com.hyundai.myexperience.UPCOMING_PROGRAM_IS_NULL
 import com.hyundai.myexperience.utils.formatScheduleDate
@@ -17,8 +18,14 @@ fun setName(view: TextView, name: String?){
 }
 
 @BindingAdapter("setLevel")
-fun setLevel(view: ImageView, level: Int){
-
+fun setLevel(view: ImageView, level: Int) {
+    val drawableId = when (level) {
+        1 -> R.drawable.mypage_iv_level_1
+        2 -> R.drawable.mypage_iv_level_2
+        3 -> R.drawable.mypage_iv_level_3
+        else -> R.drawable.mypage_iv_level_0
+    }
+    view.setImageResource(drawableId)
 }
 
 @BindingAdapter("setParticipateFinCnt")
@@ -63,12 +70,15 @@ fun setUpcomingProgramName(view: TextView, program: String){
 
 @BindingAdapter("setUpcomingProgramDate")
 fun setUpcomingProgramDate(view: TextView, date: String){
-    view.text = date // 포맷팅 필요
+    view.text = date
 }
 
 @BindingAdapter("setIfUpcomingIsNull")
 fun setIfUpcomingIsNull(view: TextView, isNull: Boolean){
     if (isNull) {
         view.visibility = View.INVISIBLE
+    }
+    else {
+        view.visibility = View.VISIBLE
     }
 }
