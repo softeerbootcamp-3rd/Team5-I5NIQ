eb1cc3ab4d391d9ea0048de94396cd26dc9ae43e
youngseo <youngseojang@gmail.com>
2024-02-24 20:00:17 +0900

    [AOS] feature: ui 적용 - 결제 상태에 따른 텍스트 색상

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/joined_program/adapter/ProgramViewHolder.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/joined_program/adapter/ProgramViewHolder.kt
@@ -1,19 +1,30 @@
 package com.hyundai.myexperience.ui.joined_program.adapter
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.hyundai.myexperience.R
 import com.hyundai.myexperience.data.entity.my_page.JoinedProgramItem
 import com.hyundai.myexperience.databinding.ItemJoinedProgramBinding
 import com.hyundai.myexperience.ui.joined_program.ProgramsItem
 
-class ProgramViewHolder (private val itemBinding: ItemJoinedProgramBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+class ProgramViewHolder(private val itemBinding: ItemJoinedProgramBinding) :
+    RecyclerView.ViewHolder(itemBinding.root) {
     fun bind(programsItem: JoinedProgramItem, itemClickListener: ProgramsItemClickListener) {
-        itemBinding.joinedProgramDate.text = programsItem.dateTime
-        itemBinding.joinedProgramTitle.text = programsItem.programName
-        itemBinding.joinedState.text = programsItem.status
-        itemBinding.rlJoinedProgramItem.setOnClickListener {
-            itemClickListener.onItemClick(
-                programsItem
-            )
+        itemBinding.apply {
+            joinedProgramDate.text = programsItem.dateTime
+            joinedProgramTitle.text = programsItem.programName
+            joinedState.text = programsItem.status
+            joinedState.setTextColor(getColorByStatus(programsItem.status))
+            rlJoinedProgramItem.setOnClickListener { itemClickListener.onItemClick(programsItem) }
         }
+    }
+
+    private fun getColorByStatus(status: String): Int {
+        return when (status) {
+            "참여완료" -> ContextCompat.getColor(itemView.context, R.color.blue)
+            "결제완료" -> ContextCompat.getColor(itemView.context, R.color.purple)
+            else -> ContextCompat.getColor(itemView.context, R.color.black)
+        }
+
     }
 }
