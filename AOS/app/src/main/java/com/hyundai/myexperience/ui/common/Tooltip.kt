187d7731fabb7081edcbd47a1e4551c2da52eadd
youngseo <youngseojang@gmail.com>
2024-02-25 16:27:00 +0900

    [AOS] feature: 일정 조회 화면 툴팁

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/common/Tooltip.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/common/Tooltip.kt
@@ -3,6 +3,7 @@ package com.hyundai.myexperience.ui.common
 import android.content.Context
 import androidx.core.content.res.ResourcesCompat
 import com.hyundai.myexperience.R
+import com.skydoves.balloon.ArrowOrientation
 import com.skydoves.balloon.Balloon
 
 fun createTooltip(context: Context, message: String): Balloon {
@@ -16,6 +17,24 @@ fun createTooltip(context: Context, message: String): Balloon {
         .setCornerRadius(4f)
         .setTextTypeface(ResourcesCompat.getFont(context, R.font.pretendard_medium)!!)
         .setAutoDismissDuration(3500L)
+        .setDismissWhenTouchOutside(true)
+        .setDismissWhenClicked(true)
+        .build()
+
+    return balloon
+}
+
+fun createTooltipOrientationTop(context: Context, message: String): Balloon {
+    val balloon = Balloon.Builder(context)
+        .setText(message)
+        .setTextColor(context.getColor(R.color.white))
+        .setTextSize(12f)
+        .setPadding(8)
+        .setBackgroundColor(context.getColor(R.color.gray7))
+        .setArrowSize(10)
+        .setArrowOrientation(ArrowOrientation.TOP)
+        .setCornerRadius(4f)
+        .setTextTypeface(ResourcesCompat.getFont(context, R.font.pretendard_medium)!!)
         .setDismissWhenTouchOutside(true)
         .setDismissWhenClicked(true)
         .build()
--- AOS/app/src/main/java/com/hyundai/myexperience/ui/main/ScheduleFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/main/ScheduleFragment.kt
@@ -10,11 +10,14 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.hyundai.myexperience.R
 import com.hyundai.myexperience.databinding.FragmentScheduleBinding
 import com.hyundai.myexperience.ui.common.adapter.PagerFragmentAdapter
+import com.hyundai.myexperience.ui.common.createTooltipOrientationTop
 import com.hyundai.myexperience.ui.notice.NoticeListActivity
+import com.skydoves.balloon.showAlignBottom
 
 class ScheduleFragment : Fragment() {
     private var _binding: FragmentScheduleBinding? = null
     private val binding get() = _binding!!
+    private val toolTipEnabled = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,10 +45,23 @@ class ScheduleFragment : Fragment() {
             val intent = Intent(requireActivity(), NoticeListActivity::class.java)
             startActivity(intent)
         }
+
+        binding.scheduleIvInfo.setOnClickListener {
+            setToolTip()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun setToolTip(){
+        binding.vTooltipPoint.showAlignBottom(
+            createTooltipOrientationTop(
+                this.requireContext(),
+                "항목을 터치해 상세한 일정을\n확인할 수 있습니다."
+            )
+        )
     }
 }
