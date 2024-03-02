778f22bce249064890f03e98866e021c822665e3
ThinkingDobby <chiho1221@naver.com>
2024-03-02 17:16:27 +0900

    [AOS] feature: ReservationProgramFragment binding 변수 추가

--- AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/car_or_date_first/ReservationDateProgramFragment.kt
+++ AOS/app/src/main/java/com/hyundai/myexperience/ui/reservation/car_or_date_first/ReservationDateProgramFragment.kt
@@ -5,16 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hyundai.myexperience.R
+import com.hyundai.myexperience.databinding.FragmentReservationProgramBinding
 
 class ReservationDateProgramFragment : Fragment() {
+    private var _binding: FragmentReservationProgramBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_reservation_date_program, container, false)
+    ): View {
+        _binding = FragmentReservationProgramBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
